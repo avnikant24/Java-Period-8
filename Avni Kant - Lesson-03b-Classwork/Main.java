d643d14b5a973c6c0ccfe2e8eece654ac4f96a35
Avni Kant <avnik3@nycstudents.net>
2026-09-22 17:48:52 +0000

    c1

---
+++ Avni Kant - Lesson-03b-Classwork/Main.java
@@ -0,0 +1,14 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+
+  // Init function which runs when the 
+  // program begins.  
+
+  void init(){
+   System.out.println("If you can change your mind, you can change your life. - William James");
+  }
+
+  
+}
